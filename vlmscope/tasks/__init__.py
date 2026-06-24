b4f046909e72a1eba26f193f05a9ef162ac2a2ac
Nathanm <291670497+nathanm82@users.noreply.github.com>
2026-06-25 03:33:47 +0800

    feat(tasks): register builtin tasks

--- vlmscope/tasks/__init__.py
+++ vlmscope/tasks/__init__.py
@@ -3,9 +3,22 @@
 from __future__ import annotations
 
 from vlmscope.registry import Registry
-from vlmscope.tasks.base import Task
+from vlmscope.tasks.base import GenerationTask, Task
+from vlmscope.tasks.captioning import CaptioningTask
+from vlmscope.tasks.retrieval import RetrievalTask
+from vlmscope.tasks.vqa import VQATask
 
 #: Registry of task instances keyed by ``Task.name``.
 task_registry: Registry[Task] = Registry("task")
+task_registry.register("vqa", VQATask())
+task_registry.register("captioning", CaptioningTask())
+task_registry.register("retrieval", RetrievalTask())
 
-__all__ = ["Task", "task_registry"]
+__all__ = [
+    "CaptioningTask",
+    "GenerationTask",
+    "RetrievalTask",
+    "Task",
+    "VQATask",
+    "task_registry",
+]
